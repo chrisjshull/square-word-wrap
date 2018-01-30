fc3bdf9a27461ef715ee5eef77e5ae2edac9c48d
Chris J. Shull <chrisjshull@gmail.com>
2018-01-30 05:08:25 -0800

    basics working

--- src/index.js
+++ src/index.js
@@ -7,10 +7,73 @@
  * ________
  */
 
+// future: grok terminal color escapes like https://github.com/chalk/chalk, https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+// future: grok hyphens, inc. soft hyphens
+// future: assume or allow passing or size of tabs
+// future: support preserving existing whitespace chars without forcing them to be spaces inc. what to do with leading and repeating whitespace
+
+import GraphemeSplitter from 'grapheme-splitter';
+
+const splitter = new GraphemeSplitter();
+
+const isWhiteSpace = (char) => /^[\t\n\r ]$/.test(char); // collapsible whitespace, like HTML/CSS
+
+const DEBUG = false;
+
 /**
  * ________
  */
-export default class Bootstrap {
+export default function squareWordWrap(str, opts = {}) {
+  const chars = splitter.splitGraphemes(str);
+  const words = [];
+  let longestWordLength = 0;
+  let charCount = 0;
+  {
+    let word = [];
+    for (let i = 0; i < chars.length; i++) {
+      const char = chars[i];
+      if (!isWhiteSpace(char)) {
+        charCount++;
+        word.push(char);
+        if (i < chars.length - 1) continue;
+      }
 
+      if (word.length) {
+        longestWordLength = Math.max(longestWordLength, word.length);
+        words.push(word);
+        word = [];
+      }
+    }
+  }
+  charCount += words.length - 1;
 
+  if (!words.length) return '';
+
+  const target = Math.max(
+    opts.longWordForcesRect ? longestWordLength : 0,
+    Math.ceil(Math.sqrt(charCount)) // chances are an extra line will be needed, so round up to columns
+  );
+
+  DEBUG && console.log({str, target, charCount, longestWordLength}, words);
+
+  const output = [];
+  const line = []; //words[0];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    DEBUG && console.log({word, line}, line.length + 1 + word.length);
+
+    if (line.length + 1 + word.length > target && line.length) {
+      output.push(line.join(''));
+      line.length = 0;
+    }
+
+    if (line.length) line.push(' ');
+    line.push(...word);
+
+    if (i === words.length - 1) output.push(line.join(''));
+  }
+
+  DEBUG && console.log({output});
+
+  return output.join('\n');
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,44 @@
 
-import Subject from './';
+import squareWordWrap from './';
 
-describe('Subject', () => {
-  it('can instantiate', () => {
-    const subject = new Subject(); // eslint-disable-line no-unused-vars
+describe('squareWordWrap', () => {
+  it('basics', () => {
+    expect(squareWordWrap('')).to.equal('');
+    expect(squareWordWrap('a')).to.equal('a');
+    expect(squareWordWrap('a b')).to.equal('a\nb');
+    expect(squareWordWrap('aa bb')).to.equal('aa\nbb');
+    expect(squareWordWrap('aaa bbb ccc')).to.equal('aaa\nbbb\nccc');
+  });
+
+  it('unbalenced', () => {
+    expect(squareWordWrap('aa')).to.equal('aa');
+    expect(squareWordWrap('a b c')).to.equal('a b\nc');
+    expect(squareWordWrap('aa bb cc')).to.equal('aa\nbb\ncc');
+
+    expect(squareWordWrap('a bb')).to.equal('a\nbb');
+    expect(squareWordWrap('a bb ccc')).to.equal('a\nbb\nccc');
+    expect(squareWordWrap('a bb ccc dddd')).to.equal('a bb\nccc\ndddd');
+  });
+
+  it('long word', () => {
+    expect(squareWordWrap('a b c ddddd')).to.equal('a b\nc\nddddd');
+  });
+
+  it('long word with longWordForcesRect', () => {
+    expect(squareWordWrap('a b c ddddd', {longWordForcesRect: true})).to.equal('a b c\nddddd');
+  });
+
+  it('collapses whitespace', () => {
+    expect(squareWordWrap(' a\nb  ccc\tddd')).to.equal('a b\nccc\nddd');
+  });
+
+  it('respects nbsp', () => {
+    expect(squareWordWrap('aa bb\u00A0cc')).to.equal('aa\nbb\u00A0cc');
+  });
+
+  it('respects graphemes', () => {
+    // just making sure dependency being used
+    // JS natively thinks that '🏳️‍🌈'.length === 6
+    expect(squareWordWrap('🏳️‍🌈 a b c')).to.equal('🏳️‍🌈 a\nb c');
   });
 });
