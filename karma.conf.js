369b18f8d18ac81f2c1d0df724c8eeda3ed4a3e3
Chris J. Shull <chrisjshull@gmail.com>
2018-02-01 02:08:11 -0800

    simplify linting

--- karma.conf.js
+++ karma.conf.js
@@ -37,9 +37,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.test.js': ['webpack'],
-      'src/**/*.js': ['eslint'],
-      '*.js': ['eslint']
+      'src/**/*.test.js': ['webpack']
+      //'src/**/*.js': ['eslint']
+      //'*.js': ['eslint']
     },
 
     // test results reporter to use
